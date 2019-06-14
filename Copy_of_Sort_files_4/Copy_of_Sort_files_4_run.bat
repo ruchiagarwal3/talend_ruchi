34ada4b110da59bb12c165ef512980dc8b30f137
ruchiagarwal3 <ruchiagarwal3@deloitte.com>
2019-06-14 16:27:24 +0530

    Intial Commit

---
+++ Copy_of_Sort_files_4/Copy_of_Sort_files_4_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/commons-collections4-4.1.jar;../lib/commons-lang3-3.7.jar;../lib/dom4j-1.6.1.jar;../lib/external_sort.jar;../lib/geronimo-stax-api_1.0_spec-1.0.1.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-8.0.12.jar;../lib/poi-3.16-20170419_modified_talend.jar;../lib/poi-ooxml-3.16-20170419_modified_talend.jar;../lib/poi-ooxml-schemas-3.16-20170419.jar;../lib/poi-scratchpad-3.16-20170419.jar;../lib/talendExcel-1.3-20180215.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;../lib/xmlbeans-2.6.0.jar;copy_of_sort_files_4_0_1.jar; sample_project.copy_of_sort_files_4_0_1.Copy_of_Sort_files_4  --context=Default %*
